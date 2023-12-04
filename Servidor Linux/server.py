befcd1c374087e4b6c34766d0866d402a5eb1efa
kellyfmachado <134961117+kellyfmachado@users.noreply.github.com>
2023-12-04 17:01:40 -0300

    Add files via upload

---
+++ Servidor Linux/server.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, Response
+import cv2
+import numpy as np
+import urllib.request
+
+app = Flask(__name__)
+
+# Replace the URL with the IP camera's stream URL
+url = 'http://192.168.43.46/cam-lo.jpg'
+
+def generate_frames():
+    while True:
+        img_resp = urllib.request.urlopen(url)
+        imgnp = np.array(bytearray(img_resp.read()), dtype=np.uint8)
+        im = cv2.imdecode(imgnp, -1)
+
+        ret, buffer = cv2.imencode('.jpg', im)
+        frame = buffer.tobytes()
+
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
+
